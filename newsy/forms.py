44e8cf440839c660345f0bdfa37725cc70e865ba
Benjamin Liles <benliles@arch.tamu.edu>
2011-10-10 10:33:49 -0500

    News item slugs must be lowercase and the 0003 data migration lowercases existing slugs

--- newsy/forms.py
+++ newsy/forms.py
@@ -1,10 +1,16 @@
-from django import forms
+from re import compile
 
-#from tagging.fields import TagField
+from django import forms
+from django.core.validators import RegexValidator
+from django.utils.translation import ugettext_lazy as _
 
 from newsy.models import NewsItem
 
 
+
+validate_slug = RegexValidator(compile(r'^[-a-z0-9]+$'),
+    u"Enter a valid 'slug' consisting of lowercase letters, numbers, "
+      "underscores or hyphens.", 'invalid')
 
 class NewsItemAddForm(forms.ModelForm):
     class Meta:
@@ -17,7 +23,7 @@ class NewsItemForm(NewsItemAddForm):
     tags = forms.CharField(widget=forms.TextInput(attrs={'size': 96}))
     short_title = forms.CharField(widget=forms.TextInput(attrs={'size': 96}))
     page_title = forms.CharField(widget=forms.TextInput(attrs={'size': 96}))
-    
+    slug = forms.CharField(required=True, validators=[validate_slug])
     
     class Meta(NewsItemAddForm.Meta):
         exclude = []
---
+++ newsy/migrations/0003_lowercase_slugs.py
@@ -0,0 +1,75 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        """ Set slug values to lowercase versions of the slug """
+        for item in orm.NewsItem.objects.all():
+            item.slug = item.slug.lower()
+            item.save()
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        'newsy.newsitem': {
+            'Meta': {'ordering': "['-publication_date', 'title']", 'object_name': 'NewsItem'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'page_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'placeholders': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['cms.Placeholder']", 'symmetrical': 'False'}),
+            'publication_date': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'}),
+            'short_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['sites.Site']", 'symmetrical': 'False'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255', 'db_index': 'True'}),
+            'tags': ('tagging.fields.TagField', [], {}),
+            'template': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'newsy.newsitemthumbnail': {
+            'Meta': {'object_name': 'NewsItemThumbnail', 'db_table': "'newsy_newsitem_thumbnail'"},
+            'crop_from': ('django.db.models.fields.CharField', [], {'default': "'center'", 'max_length': '10', 'blank': 'True'}),
+            'date_taken': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'effect': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'newsitemthumbnail_related'", 'null': 'True', 'to': "orm['photologue.PhotoEffect']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'news_item': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'thumbnail'", 'unique': 'True', 'to': "orm['newsy.NewsItem']"}),
+            'view_count': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'})
+        },
+        'photologue.photoeffect': {
+            'Meta': {'object_name': 'PhotoEffect'},
+            'background_color': ('django.db.models.fields.CharField', [], {'default': "'#FFFFFF'", 'max_length': '7'}),
+            'brightness': ('django.db.models.fields.FloatField', [], {'default': '1.0'}),
+            'color': ('django.db.models.fields.FloatField', [], {'default': '1.0'}),
+            'contrast': ('django.db.models.fields.FloatField', [], {'default': '1.0'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'filters': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
+            'reflection_size': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'reflection_strength': ('django.db.models.fields.FloatField', [], {'default': '0.59999999999999998'}),
+            'sharpness': ('django.db.models.fields.FloatField', [], {'default': '1.0'}),
+            'transpose_method': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['newsy']
